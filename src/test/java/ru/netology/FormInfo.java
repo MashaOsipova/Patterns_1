7ba370fbcd84d19441166af1134b51a68c803c45
MashaOsipova <mashaosi395@mail.ru>
2022-12-13 09:59:14 +0300

    Initial commit

---
+++ src/test/java/ru/netology/FormInfo.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+
+import lombok.Data;
+
+@Data
+public class FormInfo {
+    private final String city;
+    private final String name;
+    private final String phone;
+}
+
